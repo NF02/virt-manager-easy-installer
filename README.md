d455a77464a56556043419cff9ca86e3fdf0a949
NFVblog <66777517+NF02@users.noreply.github.com>
2022-04-08 19:36:04 +0000

    init release

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# virt-manager-dotfiles
-Script that simplifies the installation of the Virt-manager virtualization environment
+# virt-manager dotfiles
+This repo explains how to install the Virt-manager virtualization environment,
+in the most functional way possible, without excluding a practical automated
+installation script, which will set up the service on **Arch Linux**, complete with
+automatic assignment of user privileges.
+
+## Setup
+The first step is to clone the repo
+```sh
+	git clone https://github.com/NF02/virt-manager-dotfiles && cd virt-manager-dotfiles
+```
+The second step is to grant the application the execution permissions,
+to do that it is necessary to execute the command
+```sh
+	chmod +x install-virtmenager.sh
+```
+
+### EFI Firmware
+The UEFI firmware is a separate package compared to the virtualization 
+environment, and for this reason I have included it in the script so that I can
+fully exploit it. Of course the `HOST` machine can also be legacy because it
+doesn't affect the `guest` system.
+
+## Warning!!
+This script is meant to be run with an admin user, so that can use admin
+privileges via the `sudo` command, in case this utility is not installed
+the script will not work.
---
+++ install-virtmenager.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+install_\virt() {
+	sudo pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils \
+		openbsd-netcat ebtables iptables ebtables iptables edk2-ovmf
+
+	sudo systemctl enable --now libvirtd.service
+
+	sudo usermod -a -G libvirt $(whoami)
+	sudo usermod -a -G kvm $(whoami)
+
+	sudo systemctl restart libvirtd.service
+	notify-send "Done!"
+}
+
+if [[ $1 == --help ]] || [[ $1 == -h ]] || [[ $1 == -HELP ]] || [[ $1 == "" ]]; then
+	cat << 'EOF'
+This script will install virt-manager and qemu with all necessary packages, 
+obviously reading the documentation is recommended.
+----------------------------------------------------------------------------
+-                               commands                                   -
+----------------------------------------------------------------------------
+
+-y: continues without asking for consent (obviously the user's password will
+be required)
+
+EOF
+	exit 0
+fi
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
+
+if ! command_exists pacman; then
+    echo "Sorry this script is for Arch Linux only"
+    exit 1
+fi
+
+echo "Do you want to install the virt-manager environment?"
+read con
+
+if [[ $con == "yes" ]];then
+	install_virt
+fi
