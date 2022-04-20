5c5eb96c32000fd05b27bbb792b179396bb8ece9
nf02 <nicoferru190@gmail.com>
2022-04-20 12:41:13 +0200

    support debian and fedora

--- install-virtmenager.sh
+++ install-virtmenager.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
+if ! command_exists sudo; then
+	echo "sudo not install"
+fi
 install_virt() {
-	sudo pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils \
-	     openbsd-netcat ebtables iptables ebtables iptables edk2-ovmf \
-	     qemu-arch-extra archboot-qemu-aarch64
-
+    	if command_exists pacman; then
+		sudo pacman -Sy qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils \
+	     		openbsd-netcat ebtables iptables ebtables iptables edk2-ovmf \
+	     		qemu-arch-extra archboot-qemu-aarch64
+	else if command_exists apt; then
+		 apt install libvirt-deamon virt-manager qemu
+	     else if command_exists dnf; then
+		      sudo dnf install @virtualization
+		      else echo "I'm sorry"
+		  fi
+	     fi
+	fi
 	sudo systemctl enable --now libvirtd.service
 
 	sudo usermod -a -G libvirt $(whoami)
@@ -27,18 +41,13 @@ be required)
 EOF
 	exit 0
 fi
-command_exists() {
-  command -v "$@" >/dev/null 2>&1
-}
 
-if ! command_exists pacman; then
-    echo "Sorry this script is for Arch Linux only"
-    exit 1
-fi
+
 if [[ $1 == "-y" ]];then
 	install_virt
 	exit 0
 fi
+
 echo "Do you want to install the virt-manager environment?"
 read con
 
