a2361a310111500c728a63687956f6a4ed18adc1
NFVblog <nicoferru190@gmail.com>
2024-04-19 16:25:36 +0200

    GNS3 installer for Arch Linux
    the script has been created to install the gns3 networking environment, this script installs all the packages necessary to have a "ready to use" configuration without problems due to the lack of some simulator dependencies. for further information, the author recommends looking at the Arch documentation. The author is not responsible for inadvertent damage to other people's computer systems

---
+++ install-gns3.sh
@@ -0,0 +1,50 @@
+#!/bin/env sh
+# Author: NFVblog <ask dot nfvblog at Outlook dot it>
+
+# Check
+command_exists() {
+    command -v "$@" >/dev/null 2>&1
+}
+
+if ! command_exists sudo; then
+    echo "sudo not install"
+    exit -1 
+fi
+
+if ! command_exists git; then
+    echo "git not install"
+    exit -3
+fi
+
+if ! command_exists yay; then
+    cd /tmp && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+fi
+
+# Install GNS3 by YAY AUR Helper
+install_gns3(){
+    yay -S busybox python-aiofiles python-aiohttp python-aiohttps-cors \
+	python-async-timeout python-distro python-jsonschema \
+	python-platformdirs python-psutil python-py-cpuinfo python-sentry_sdk \
+	python-setuptools python-truststore dynamips ubridge vpcs gns3-gui \
+	gns3-server
+}
+
+cat << 'EOF'
+┏━╸┏┓╻┏━┓╻ ╻   ╻┏┓╻┏━┓╺┳╸┏━┓╻  ╻  ┏━╸┏━┓
+┃╺┓┃┗┫┗━┓┗━┫   ┃┃┗┫┗━┓ ┃ ┣━┫┃  ┃  ┣╸ ┣┳┛
+┗━┛╹ ╹┗━┛  ╹   ╹╹ ╹┗━┛ ╹ ╹ ╹┗━╸┗━╸┗━╸╹┗╸
+--------------------------------------------------------------
+ * install GNS3 server and GUI;
+ * dynamips support;
+ * vpcs support;
+ * ubridge support.
+-------------------------------------------------------------- 
+Do you want to install the GNS3 environment (Yes or No)?
+EOF
+read con
+
+if [[ $con == "yes" ]] || [[ $con == "y" ]];then
+   	install_gns3
+   	exit 0
+   else echo "exit!!!"
+fi
